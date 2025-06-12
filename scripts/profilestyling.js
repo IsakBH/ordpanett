e130ac8180ee5ced405a99be91e43bafc4e115f7
isakbh <isak@brunhenriksen.net>
2025-06-12 15:48:23 +0200

    Begge brukerne til Lucas er nå gylden. Vet ikke hvilken av de han bruker så...

--- scripts/profilestyling.js
+++ scripts/profilestyling.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // sjekker om user_id er lucas sin
-    if (user_id === 38) {
+    if (user_id === 38 || 44) {
         console.log("Hei Lucas! Dette er Herr Ord På Nett som snakker. Siden du har vært så grei og glazet Ord På Nett såpass mye, skal du få æren av å ha en gylden profil. Takk for din innsats i dette prosjektet hvor jeg tar over den digitale verden sakte men sikkert!")
         const profileElement = document.querySelector('.profile-settings');
         if (profileElement) {
