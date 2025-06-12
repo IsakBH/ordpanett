2a5e8f334ad71f8d026e57c4e4f929a58c203d1d
IsakBH <isak@brunhenriksen.net>
2025-06-12 18:11:48 +0200

    Profil siden applyer nå dark mode

--- pages/profile.php
+++ pages/profile.php
@@ -21,6 +21,7 @@ echo "<script>const user_id = " . json_encode($user_id) . ";</script>";
     <link rel="stylesheet" href="../styling/texteditor.css">
     <script src="../scripts/texteditor.js"></script>
     <script src="../scripts/profilestyling.js"></script>
+<script src="../scripts/applydarkmode.js"></script>
     <link rel="icon" href="../assets/ordlogo.png" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
