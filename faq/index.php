137d73673f330bbc4173488bc5a87d2badfbbc94
IsakBH <isak@brunhenriksen.net>
2025-09-03 08:17:57 +0200

    Såvidt begynt på en FAQ til Ord På Nett

---
+++ faq/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ord På Nett | FAQ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="styling/texteditor.css" />
+    <link rel="icon" href="../assets/ordlogo.png" />
+    <!-- ikoner fra font awesome og google fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <h1>FAQ</h1>
+    <?php
+    include("questions.html");
+    ?>
+</body>
