562842fe85a56646e077c6623d7a5e468c18be4f
isakbh <isak@brunhenriksen.net>
2025-10-22 23:17:31 +0200

    applyGreenMode() på login og fikset border-color på input type color på dark mode

--- pages/login.php
+++ pages/login.php
@@ -72,7 +72,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Ord På Nett | Logg inn</title>
     <link rel="stylesheet" href="../styling/texteditor.css">
     <script src="../scripts/texteditor.js"></script>
-<script src="../scripts/applydarkmode.js"></script>
+    <script src="../scripts/applydarkmode.js"></script>
+    <script src="../scripts/applygreenmode.js"></script>
     <link rel="icon" href="../assets/ordlogo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
