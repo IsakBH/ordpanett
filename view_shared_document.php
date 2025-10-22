ff1822150fef4169414c8c49a56e9c88b31fe69e
IsakBH <isak@brunhenriksen.net>
2025-10-22 11:39:11 +0200

    legger til script for å aktivere float mode i delte dokumenter

--- view_shared_document.php
+++ view_shared_document.php
@@ -27,6 +27,7 @@ if (!$document) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <script src="./scripts/texteditor.js"></script>
     <script src="./scripts/applydarkmode.js"></script>
+    <script src="./scripts/applyfloatmode.js"></script>
     <link rel="icon" href="./assets/ordlogo.png" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
 </head>
