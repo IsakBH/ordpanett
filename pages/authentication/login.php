d11a264bea2717b02bc7be15baa31354c11eb49a
isakbh <isak@brunhenriksen.net>
2026-05-08 21:42:21 +0200

    begynnelse på login

--- pages/authentication/login.php
+++ pages/authentication/login.php
@@ -10,7 +10,17 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
 <!DOCTYPE html>
 <html lang="no">
     <html>
+        <title>Ord På Nett - Logg inn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="../../styles/authentication.css">
+
+        <!-- Ikoner fra Font Awesome og Google Fonts -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     </html>
 
     <body>
