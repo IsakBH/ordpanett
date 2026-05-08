50fa92fc4f5fd66e73b423dcbe9f7e14cfe77f74
isakbh <isak@brunhenriksen.net>
2026-05-08 21:39:18 +0200

    endret navn på auth mappen til authentication og authentication.css

---
+++ pages/authentication/login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once __DIR__ . '../../config/database.php';
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="no">
+    <html>
+
+    </html>
+
+    <body>
+
+    </body>
+</html>
